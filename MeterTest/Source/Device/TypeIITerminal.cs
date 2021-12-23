ce2263bf7a362b4abaae2f4b77fc9e4ab9a9721b
水无痕 <447803771@qq.com>
2021-12-23 16:20:32 +0800

    [CHG]:升级流程中引入多次升级，提供升级成功率；

--- MeterTest/Source/Device/TypeIITerminal.cs
+++ MeterTest/Source/Device/TypeIITerminal.cs
@@ -47,12 +47,24 @@ namespace MeterTest.Source.Device
         }
         public void SetUpdateFlag()
         {
-            DataId dataId = new DataId(0xA5A10004, 0xA55AA5A5);
+            DataId dataId = new DataId(0xA5A10004, 0xAA55A55A);
             client.WriteRepInogreTimeOut(Address, dataId, Password, OperatorCode);
         }
         public void McuReset()
         {
             DataId dataId = new DataId(0xA5A10005, 0x12345678);
+            client.WriteRepInogreTimeOut(Address, dataId, Password, OperatorCode);
+        }
+        private uint GetCalChk()
+        {
+            DataId dataId = new DataId(0xA5A10006);
+            byte[] data = client.ReadRepInogreTimeOut(Address, dataId);
+            uint rst = (uint)(((uint)data[0] * 256 + data[1]) * 256 + data[2]) * 256 + data[3];
+            return rst;
+        }
+        private void StartCalChk()
+        {
+            DataId dataId = new DataId(0xA5A10007, 0x01);
             client.WriteRepInogreTimeOut(Address, dataId, Password, OperatorCode);
         }
         public void ExFlashWrite(byte[] buff, int offset, int len)
@@ -62,14 +74,10 @@ namespace MeterTest.Source.Device
             DataId dataId = new DataId(id, memoryStream.ToArray());
             client.WriteRepInogreTimeOut(Address, dataId, Password, OperatorCode);
         }
-        public override void SoftwareUpdate(string updateFilePath)
+        private bool WriteUpdateSoftware(string updateFilePath)
         {
-            string versionBefore = GetVersion();
-            Log.SendDeviceLog("当前软件版本号：" + versionBefore);
-            SetFactoryStatus(FactoryStatus.FactoryIn);
             byte[] bytes = File.ReadAllBytes(updateFilePath);
             int singleLen = 0;
-            uint check = PublicClass.Crc32(bytes, bytes.Length, 0);
             for (int i = 0; i < bytes.Length; i += WRITE_LEN)
             {
                 if(((i % (4 * 1024)) == 0) || ((i % (4 * 1024) + WRITE_LEN) >= (4 * 1024)))
@@ -82,11 +90,27 @@ namespace MeterTest.Source.Device
             }
             Log.SendDeviceLog("正在写入升级程序，已写入 100%");
             SetUpdateSoftLen(bytes.Length);
-            SetUpdateSoftwareCheck(PublicClass.Crc32(bytes, bytes.Length, 0));
+            uint check = PublicClass.Crc32(bytes, bytes.Length, 0);
+            SetUpdateSoftwareCheck(check);
+            StartCalChk();
+            Thread.Sleep(5000);
+            return check == GetCalChk();
+        }
+        public override void SoftwareUpdate(string updateFilePath)
+        {
+            string versionBefore = GetVersion();
+            Log.SendDeviceLog("当前软件版本号：" + versionBefore);
+            SetFactoryStatus(FactoryStatus.FactoryIn);
+            SetUpdateFlag();
+            int cnt = 0;
+            while((cnt < 5) && (!WriteUpdateSoftware(updateFilePath)))
+            {
+                cnt++;
+            }
             SetUpdateFlag();
             McuReset();
 
-            int cnt = 0;
+            cnt = 0;
             string versionAft = null;
             while(cnt < 15)
             {
